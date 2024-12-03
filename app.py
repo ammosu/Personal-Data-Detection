badba65b920f85cabc5db9201cceb7659801777a
ammosu <pang2258@gmail.com>
2024-12-03 14:39:52 +0800

    update app.py & index.html

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def analyze_with_openrouter(text):
             OPENROUTER_API_URL,
             headers=headers,
             json={
-                "model": "openai/gpt-3.5-turbo",  # 可換成其他模型
+                "model": "meta-llama/llama-3.1-70b-instruct:free",  # 可換成其他模型
                 "messages": [{"role": "user", "content": prompt}]
             }
         )
